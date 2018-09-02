452e5bc3f481db492ab1ae17fa28170e4035238d
Phillip Sun <psun.04@gmail.com>
2018-09-02 13:05:35 -0500

    cleaned up routes and reducer files

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,50 +1,53 @@
-const initialState = {
+export const initialState = {
   id: 0,
-  email: '',
-  name: '',
-  make: '',
-  model: '',
+  email: "",
+  name: "",
+  make: "",
+  model: "",
   year: 0,
   mileage: 0,
-  img: '',
-  description: '',
+  img: "",
+  description: "",
   price: 0,
   listings: [],
   userListings: [],
-  vin: ''
-}
+  vin: ""
+};
 
-const SET_LISTING_INFO = 'SET_LISTING_INFO';
-const INITIALIZE_USER = 'INITIALIZE_USER';
-const CLEAR_INFO = 'CLEAR_INFO';
-const FETCH_ALL = 'FETCH_ALL';
-const INITIALIZE_USER_LISTINGS = 'INITIALIZE_USER_LISTINGS';
-const UPDATE_USER_LISTINGS = 'UPDATE_USER_LISTINGS';
-const LOGOUT = 'LOGOUT';
-const DELETE_LISTING = 'DELETE_LISTING';
-const UPDATE_LISTING = 'UPDATE_LISTING';
+const SET_LISTING_INFO = "SET_LISTING_INFO";
+const INITIALIZE_USER = "INITIALIZE_USER";
+const CLEAR_INFO = "CLEAR_INFO";
+const FETCH_ALL = "FETCH_ALL";
+const INITIALIZE_USER_LISTINGS = "INITIALIZE_USER_LISTINGS";
+const UPDATE_USER_LISTINGS = "UPDATE_USER_LISTINGS";
+const LOGOUT = "LOGOUT";
+const DELETE_LISTING = "DELETE_LISTING";
+const UPDATE_LISTING = "UPDATE_LISTING";
 
 export default function reducer(state = initialState, action) {
   let { type, payload } = action;
-  switch(type) {
+  switch (type) {
     case INITIALIZE_USER:
-      return { ...state, ...payload};
+      return { ...state, ...payload };
     case SET_LISTING_INFO:
-      return { ...state, ...payload};
+      return { ...state, ...payload };
     case CLEAR_INFO:
       return payload;
     case FETCH_ALL:
-      return { ...state, listings: payload};
+      return { ...state, listings: payload };
     case INITIALIZE_USER_LISTINGS:
-      return { ...state, userListings: payload};
+      return { ...state, userListings: payload };
     case UPDATE_USER_LISTINGS:
-      return { ...state, userListings: payload};
+      return { ...state, userListings: payload };
     case LOGOUT:
       return initialState;
     case DELETE_LISTING:
-      return { ...state, userListings: state.userListings.filter( e => e.id !== payload) }
+      return {
+        ...state,
+        userListings: state.userListings.filter(e => e.id !== payload)
+      };
     case UPDATE_LISTING:
-      return { ...state, ...payload};
+      return { ...state, ...payload };
     default:
       return state;
   }
@@ -54,60 +57,60 @@ export function setListingInfo(listingInfo) {
   return {
     type: SET_LISTING_INFO,
     payload: listingInfo
-  }
+  };
 }
 
 export function initializeUser(user) {
   return {
-     type: INITIALIZE_USER,
-     payload: user
-  } 
+    type: INITIALIZE_USER,
+    payload: user
+  };
 }
 
 export function clear() {
   return {
     type: CLEAR_INFO,
     payload: initialState
-  }
+  };
 }
 
 export function fetchAllListings(listings) {
   return {
     type: FETCH_ALL,
     payload: listings
-  }
+  };
 }
 
 export function initializeUserListings(listings) {
   return {
     type: INITIALIZE_USER_LISTINGS,
     payload: listings
-  }
+  };
 }
 
 export function updateUserListings(listings) {
   return {
     type: UPDATE_USER_LISTINGS,
     payload: listings
-  }
+  };
 }
 
 export function logout() {
   return {
     type: LOGOUT
-  }
+  };
 }
 
 export function deleteListing(listingId) {
   return {
     type: DELETE_LISTING,
     payload: listingId
-  }
+  };
 }
 
 export function updateListing(listing) {
   return {
     type: UPDATE_LISTING,
     payload: listing
-  }
+  };
 }
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 // Import components
-import Auth from './component/Auth/Auth';
-import Dashboard from './component/Dashboard/Dashboard';
-import Form from './component/Form/Form';
-import ListingDetails from './component/ListingDetails/ListingDetails';
-import UserListings from './component/UserListings/UserListings';
-import UpdateListing from './component/UpdateListing/UpdateListing';
+import Auth from "./component/Auth/Auth";
+import Dashboard from "./component/Dashboard/Dashboard";
+import Form from "./component/Form/Form";
+import ListingDetails from "./component/ListingDetails/ListingDetails";
+import UserListings from "./component/UserListings/UserListings";
+import UpdateListing from "./component/UpdateListing/UpdateListing";
 
 export default (
   <Switch>
-    <Route exact path='/' component={Auth} />
-    <Route path='/dashboard' component={Dashboard} />
-    <Route path='/listing/new' component={Form} />
-    <Route path='/listing/:id/edit' component={UpdateListing} />
-    <Route exact path='/listing/:id' component={ListingDetails} />
-    <Route path='/listings/:user_id' component={UserListings} />
+    <Route exact path="/" component={Auth} />
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/listing/new" component={Form} />
+    <Route path="/listing/:id/edit" component={UpdateListing} />
+    <Route path="/listing/:id" component={ListingDetails} />
+    <Route path="/listings/:user_id" component={UserListings} />
   </Switch>
-)
+);
