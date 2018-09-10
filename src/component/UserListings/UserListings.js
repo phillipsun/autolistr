56395fd3412a46540be49022867161696babdbad
Phillip Sun <psun.04@gmail.com>
2018-09-10 15:00:21 -0500

    fixed user listing component

--- src/component/UserListings/UserListings.js
+++ src/component/UserListings/UserListings.js
@@ -1,58 +1,71 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { initializeUserListings, updateUserListings } from './../../redux/reducer';
+import React, { Component } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import {
+  initializeUserListings,
+  updateUserListings
+} from "./../../redux/reducer";
 
 // Import Components
-import Listing from '../Listing/Listing';
+import Listing from "../Listing/Listing";
 
 // Import Styles
-import '../UserListings/UserListings.css'
+import "../UserListings/UserListings.css";
 
-class UserListing extends Component {
-  componentWillMount() {
-    this.grabListings();
+class UserListings extends Component {
+  componentDidMount() {
+    this.getListings();
   }
 
-  grabListings() {
-    console.log("Grabbing Listings...");
-    axios.get(`/api/listings/${this.props.user_id}`)
-      .then(response => {
-        //console.log("From component", response.data);
-        this.props.initializeUserListings(response.data);
-      })
+  getListings() {
+    console.log("Getting User Listings...");
+    axios.get(`/api/listings/${this.props.user_id}`).then(response => {
+      this.props.initializeUserListings(response.data);
+    });
   }
 
   render() {
-    //console.log(this.props.userListings);
     if (this.props.userListings.length > 0) {
       return (
-        <div className='user-listings'>
-          <div className='user-listings__container'>
-            <h2 className='user-listings__headline'>My Vehicle Listings:</h2>
-            {this.props.userListings.map( e => {
-                //console.log(this.props.userListings);
-                return <Listing listing={e} key={e.id} showEdit={true} showContact={false}/>
-              })}
-            </div>
+        <div className="user-listings">
+          <div className="user-listings__container">
+            <h2 className="user-listings__headline">My Vehicle Listings:</h2>
+            {this.props.userListings.map(e => {
+              return (
+                <Listing
+                  listing={e}
+                  key={e.id}
+                  showEdit={true}
+                  showContact={false}
+                />
+              );
+            })}
+          </div>
         </div>
-        )
+      );
     } else {
-      return ( <div className='user-listings'><h2 className='user-listings__headline'>No Listings to display</h2></div> )
+      return (
+        <div className="user-listings">
+          <h2 className="user-listings__headline">No Listings to display</h2>
+        </div>
+      );
     }
   }
-
-
-
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators( { initializeUserListings, updateUserListings }, dispatch )
+  return bindActionCreators(
+    { initializeUserListings, updateUserListings },
+    dispatch
+  );
 }
 
 function mapStateToProps(state) {
   return state;
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserListing);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserListings);
