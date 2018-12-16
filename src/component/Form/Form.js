ee551fea64a66510562e35a89cd47c776d85519d
Phillip Sun <psun.04@gmail.com>
2018-12-16 00:49:11 -0600

    tweaks to app

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,89 +1,153 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { setListingInfo } from './../../redux/reducer';
+import React, { Component } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import { setListingInfo } from "./../../redux/reducer";
 
 // Import Styles
-import '../Form/Form.css'
+import "../Form/Form.css";
 
 class Form extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      make: '',
-      model: '',
+      make: "",
+      model: "",
       year: 0,
       mileage: 0,
-      img: '',
-      description: '',
+      img: "",
+      description: "",
       price: 0,
-      vin: ''
-    }
+      vin: ""
+    };
 
-    this.handleChange = this.handleChange.bind(this);
     this.handleCreate = this.handleCreate.bind(this);
   }
 
-  handleChange(prop, value) {
-    this.setState({ [prop]: value })
-  }
+  // handleChange(prop, value) {
+  //   this.setState({ [prop]: value })
+  // }
+
+  handleChange = (prop, value) => this.setState({ [prop]: value });
 
   handleCreate() {
     let { user_id } = this.props;
     let listing = { ...this.state, user_id };
     //console.log(listing);
-    axios.post('/api/listing/new', listing)
-      .then(response => {
-        //this.props.clear();
-        this.props.history.push(`/Listings/${user_id}`)
-      });
+    axios.post("/api/listing/new", listing).then(response => {
+      //this.props.clear();
+      this.props.history.push(`/Listings/${user_id}`);
+    });
   }
 
   render() {
     return (
-      <div className='form'>
-        <h1 className='form__headline'>List a vehicle for sale</h1>
-        <div className='form__field-container'>
-          <div className='form__input-container'>
-            <p>Make: </p><input value={this.state.make} onChange={e => this.handleChange('make', e.target.value)} />
+      <div className="form">
+        <h1 className="form__headline">List a vehicle for sale</h1>
+        <div className="form__field-container">
+          <div className="form__input-container">
+            <p>Make: </p>
+            <input
+              value={this.state.make}
+              onChange={e => this.handleChange("make", e.target.value)}
+            />
           </div>
-          <div className='form__input-container'>
-            <p>Model: </p><input value={this.state.model} onChange={e => this.handleChange('model', e.target.value)} />
+          <div className="form__input-container">
+            <p>Model: </p>
+            <input
+              value={this.state.model}
+              onChange={e => this.handleChange("model", e.target.value)}
+            />
           </div>
-          <div className='form__input-container'>
-            <p>Year: </p><input value={this.state.year} onChange={e => this.handleChange('year', e.target.value)} />
+          <div className="form__input-container">
+            <p>Year: </p>
+            <input
+              value={this.state.year}
+              onChange={e => this.handleChange("year", e.target.value)}
+            />
           </div>
-          <div className='form__input-container'>
-            <p>Mileage: </p><input value={this.state.mileage} onChange={e => this.handleChange('mileage', e.target.value)} />
+          <div className="form__input-container">
+            <p>Mileage: </p>
+            <input
+              value={this.state.mileage}
+              onChange={e => this.handleChange("mileage", e.target.value)}
+            />
           </div>
-          <div className='form__input-container'>
-            <p>ImgUrl: </p><input value={this.state.img} onChange={e => this.handleChange('img', e.target.value)} />
+          <div className="form__input-container">
+            <p>ImgUrl: </p>
+            <input
+              value={this.state.img}
+              onChange={e => this.handleChange("img", e.target.value)}
+            />
           </div>
-          <div className='form__input-container'>
-            <p>Description: </p><input value={this.state.description} onChange={e => this.handleChange('description', e.target.value)} />
+          <div className="form__input-container">
+            <p>Description: </p>
+            <input
+              value={this.state.description}
+              onChange={e => this.handleChange("description", e.target.value)}
+            />
           </div>
-          <div className='form__input-container'>
-            <p>Price: </p><input value={this.state.price} onChange={e => this.handleChange('price', e.target.value)} />
+          <div className="form__input-container">
+            <p>Price: </p>
+            <input
+              value={this.state.price}
+              onChange={e => this.handleChange("price", e.target.value)}
+            />
           </div>
-          <div className='form__input-container'>
-            <p>VIN: </p><input value={this.state.vin} onChange={e => this.handleChange('vin', e.target.value)} />
+          <div className="form__input-container">
+            <p>VIN: </p>
+            <input
+              value={this.state.vin}
+              onChange={e => this.handleChange("vin", e.target.value)}
+            />
           </div>
         </div>
-        <div className='form__btn-container'>
-          <button className="form__create-btn" onClick={ () => {
-            this.props.setListingInfo(this.state);
-            this.handleCreate();
-          }}>Create Listing</button>
+        <div className="form__btn-container">
+          <button
+            className="form__create-btn"
+            onClick={() => {
+              this.props.setListingInfo(this.state);
+              this.handleCreate();
+            }}
+          >
+            Create Listing
+          </button>
         </div>
       </div>
-    )
+    );
   }
 }
 
 function mapStateToProps(reduxState) {
-  let { make, model, year, mileage, img, description, price, vin, user_id, email, name } = reduxState;
-  return { make, model, year, mileage, img, description, price, vin, user_id, email, name };
+  let {
+    make,
+    model,
+    year,
+    mileage,
+    img,
+    description,
+    price,
+    vin,
+    user_id,
+    email,
+    name
+  } = reduxState;
+  return {
+    make,
+    model,
+    year,
+    mileage,
+    img,
+    description,
+    price,
+    vin,
+    user_id,
+    email,
+    name
+  };
 }
 
-export default connect(mapStateToProps, { setListingInfo })(Form);
+export default connect(
+  mapStateToProps,
+  { setListingInfo }
+)(Form);
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
-import { unregister } from './registerServiceWorker';
-import ReduxPromise from 'redux-promise';
-import reducer from './redux/reducer';
-import { createStore, applyMiddleware } from 'redux';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import { unregister } from "./registerServiceWorker";
+import ReduxPromise from "redux-promise";
+import reducer from "./redux/reducer";
+import { createStore, applyMiddleware } from "redux";
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
-
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducer)}>
     <Router>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Router>
-  </Provider>
-  , document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 unregister();
